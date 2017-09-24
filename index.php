d91a497bbcb7e243b51f730bbcada8d4d0791e53
corpjorge <corpjorge@hotmail.com>
2017-09-24 00:07:28 -0500

    commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+header("Location: http://cjj.corpjorge.com/public/"); /* Redirección del navegador */
+
+/* Asegurándonos de que el código interior no será ejecutado cuando se realiza la redirección. */
+exit;
+?>
